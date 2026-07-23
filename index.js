f125cd8e0e202dc98e028b6efe48afd9eeb221d3
sxemzreyko <sxemzreyko@gmail.com>
2026-07-23 17:27:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api'); { command: 'game', description: 'Играть в Камень-Ножницы-Бумага' },
+const TelegramBot = require('node-telegram-bot-api');bot.setMyCommands([ { command: 'game', description: 'Играть в Камень-Ножницы-Бумага' },
   { command: 'compliment', description: 'Получить комплимент' },
   { command: 'roast', description: 'Получить подкол' },
   { command: 'fortune', description: 'Гадание на имени (напиши имя после команды)' },
@@ -25,7 +25,7 @@ bot.onText(/\/help/, (msg) => {
 
 👑 Только для владельца:
 /online, /offline, /status, /setmess
-bot.setMyCommands([
+
  
 
 const TOKEN = process.env.BOT_TOKEN;
